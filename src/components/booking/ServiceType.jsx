1afb9768397d84075f911f6a139bc0606d5451f7
Hannaneh Habibpour <hhabibpour@mySeneca.ca>
2023-03-13 21:25:08 -0400

    created serviceType section  component elements

--- src/components/booking/ServiceType.jsx
+++ src/components/booking/ServiceType.jsx
@@ -1,6 +1,16 @@
+import TypeList from "./shared/TypeList"
+import TypeDescription from "./shared/TypeDescription"
+
 function ServiceType() {
+
+  const serviceTypes = [{id : 0, name: "Show Room"}, {id : 1, name: "Basic"}, {id : 2, name: "Interior"}, {id : 3, name: "Exterior"}]
+
   return (
-    <div>ServiceType</div>
+    <section id="service-type">
+      <h3>Service Type</h3>
+      <TypeList types={serviceTypes} />
+      <TypeDescription />
+    </section>
   )
 }
 
