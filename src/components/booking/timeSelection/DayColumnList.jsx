adb08587d48ec381435f06511f9decb37b45bd95
Hannaneh Habibpour <hhabibpour@mySeneca.ca>
2023-03-15 18:50:01 -0400

    updated DayColumnList

--- src/components/booking/timeSelection/DayColumnList.jsx
+++ src/components/booking/timeSelection/DayColumnList.jsx
@@ -2,7 +2,7 @@ import DayColumn from "./DayColumn"
 
 function DayColumnList() {
   return (
-    <div className="grid grid-cols-3 gap-2 w-full justify-between  text-center">
+    <div className="grid grid-cols-3 gap-2 w-full justify-between text-center">
         <DayColumn date='2023-01-20'/>
         <DayColumn date='2023-01-21'/>
         <DayColumn date='2023-01-22'/>
