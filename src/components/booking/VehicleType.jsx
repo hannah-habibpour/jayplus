07df7f53ac5703450af1aecd87d6ee1add5e2515
Hannaneh Habibpour <hhabibpour@mySeneca.ca>
2023-03-17 18:57:58 -0400

    added state to TypeList and VehicleType

--- src/components/booking/VehicleType.jsx
+++ src/components/booking/VehicleType.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 import TypeList from "./shared/TypeList"
 import TypeDescription from "./shared/TypeDescription"
 
@@ -5,10 +6,12 @@ function VehicleType() {
   
   const vehicleTypes = [{id : 0, name: "Sedan"}, {id : 1, name: "SUV"}, {id : 2, name: "Large SUV / Truck"}, {id : 3, name: "Motorcycle"}]
 
+  const [ type, setType ] = useState("")
+
   return (
     <section id="vehicle-type">
       <h3>Vehicle Type</h3>
-      <TypeList types={vehicleTypes} />
+      <TypeList types={vehicleTypes} select={(type) => setType(type)} />
       <TypeDescription />
     </section>
   )
--- src/components/booking/shared/TypeList.jsx
+++ src/components/booking/shared/TypeList.jsx
@@ -1,11 +1,26 @@
+import { useState } from 'react'
 import Button from "../../shared/all/Button"
 
-function TypeList({ types }) {
+
+function TypeList({ types, select }) {
+
+  const [ selected, setSelected ] = useState("Sedan")
+
+  const handleChange = (e) => {
+    setSelected(e.currentTarget.value)
+    select(e.currentTarget.value)
+  }
+
   return (
     <div id="type-list" className="flex gap-4">
-        {types.map( type => (
-          <Button key={type.id}>{type.name}</Button>
-        ))}
+        <ul className='flex gap-4'>
+          {types.map( type => (
+            <li key={ type.id }>
+              <input type="radio" id={type.id} name="type" value={type.name} onChange={handleChange} checked={ selected ===  type.name } />
+              <label htmlFor={type.id}>{type.name}</label>
+            </li>
+          ))}
+        </ul>
     </div>
   )
 }
