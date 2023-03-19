eb494148908e9e239b2239f72263c501b343715d
Hannaneh Habibpour <hhabibpour@mySeneca.ca>
2023-03-19 14:52:19 -0400

    moved context to the app level

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BookingProvider } from './context/BookingContext';
+
 import Booking from './pages/Booking';
 import Payment from './pages/Payment';
 import PaymentSuccess from './pages/PaymentSuccess';
 
 function App() {
   return (
-    <Router>
-      <main>
-          <Routes>
-            <Route path='/' element={<Booking />} />
-            <Route path='/payment' element={<Payment />} />
-            <Route path='/payment-success' element={<PaymentSuccess />} />
-          </Routes>
-      </main>
-    </Router>
+    <BookingProvider>
+      <Router>
+        <main>
+            <Routes>
+              <Route path='/' element={<Booking />} />
+              <Route path='/payment' element={<Payment />} />
+              <Route path='/payment-success' element={<PaymentSuccess />} />
+            </Routes>
+        </main>
+      </Router>
+    </BookingProvider>
   );
 }
 
--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -3,18 +3,14 @@ import ServiceType from "../components/booking/ServiceType"
 import TimeSelection from "../components/booking/TimeSelection"
 import BookingFooter from "../components/booking/BookingFooter"
 
-import { BookingProvider } from "../context/BookingContext"
-
 function Booking() {
   return (
-    <BookingProvider>
       <div id="booking-page">
         <VehicleType />
         <ServiceType />
         <TimeSelection />
         <BookingFooter />
       </div>
-    </BookingProvider>
   )
 }
 
