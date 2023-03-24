7a47672e7a2fc136ea51ebdea3142a57673edbde
Hannaneh Habibpour <hhabibpour@mySeneca.ca>
2023-03-24 12:47:12 -0400

    removed settings component from index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import Settings from './Settings'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Settings />
     <App />
   </React.StrictMode>
 );
