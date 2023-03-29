859954d2abd1855c446658aaeacc3da0f79a1dbf
Hannaneh Habibpour <hhabibpour@mySeneca.ca>
2023-03-29 15:48:55 -0400

    Styled the TypeDescription component

--- src/components/booking/shared/TypeDescription.jsx
+++ src/components/booking/shared/TypeDescription.jsx
@@ -1,6 +1,15 @@
 function TypeDescription({ description }) {
+
+  const descriptionStyle = {
+    background: process.env.REACT_APP_DESCRIPTION_CARD_BACKGROUND_COLOR,
+    borderStyle: "solid",
+    borderColor: process.env.REACT_APP_DESCRIPTION_CARD_BORDER_COLOR,
+    borderWidth: "2px",
+    borderRadius: "5px",
+    padding: "15px",
+  }
   return (
-    <div>{description}</div>
+    <div style={descriptionStyle}>{description}</div>
   )
 }
 
