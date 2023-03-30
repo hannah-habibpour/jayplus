6647583d57575901840bfec646e80eb3c28e2294
Hannaneh Habibpour <hhabibpour@mySeneca.ca>
2023-03-29 20:38:34 -0400

    Wrote comments for VehicleType and TypeList

--- src/components/booking/VehicleType.jsx
+++ src/components/booking/VehicleType.jsx
@@ -5,11 +5,13 @@ import TypeDescription from "./shared/TypeDescription"
 import BookingContext from "../../context/BookingContext"
 
 function VehicleType() {
-  
+  // Function that retreives the vehicle types from the API
   const vehicleTypes = getVehicleTypes()
 
+  // Context we are using to store the state of the selected vehicle type
   const { vehicleTypeSelected, setVehicleTypeSelected } = useContext(BookingContext)
 
+  // Function returns a description of the vehicle type selected or "No description found."
   const getDescription = () => {
     const matchingType = vehicleTypes.types.find(t => t.name === vehicleTypeSelected);
     return matchingType ? matchingType.description : "No description found.";
@@ -18,14 +20,13 @@ function VehicleType() {
   return (
     <section id="vehicle-type">
       <div className="w-full">
-
-      <h3>Vehicle Type</h3>
-      <TypeList
-        types={vehicleTypes}
-        select={(type) => {
+        <h3>Vehicle Type</h3>
+        <TypeList
+          types={vehicleTypes}
+          select={(type) => {
           setVehicleTypeSelected(type)
         }} />
-      <TypeDescription description={getDescription()}/>
+        <TypeDescription description={getDescription()} />
       </div>
     </section>
   )
--- src/components/booking/shared/TypeList.jsx
+++ src/components/booking/shared/TypeList.jsx
@@ -5,11 +5,16 @@ import { calculatePrice } from "../../../utilities/utils"
 
 function TypeList({ types, select }) {
 
-  const [ selected, setSelected ] = useState(types.types[0].name)
+  // Set the default selected type to the first type in the types list
+  const [selected, setSelected] = useState(types.types[0].name)
+  // Function received as a prop from the parent component that is called when a type is selected.
+  // Function passes the state to parent component, which then sets the state in context.
   select(selected)
 
+  // State to determine if the type is hovered over
   const [isHovered, setIshovered] = useState(null)
   
+  // Styles for the list item
   const liStyle = {
     position: "relative",
     background: process.env.REACT_APP_INACTIVE_INPUT_BUTTON_COLOR,
@@ -21,6 +26,7 @@ function TypeList({ types, select }) {
     transition: "0.3s",
   }
 
+  // Styles for the list item when hovered over
   const liHoveredStyle = {
     position: "relative",
     background: process.env.REACT_APP_ACTIVE_INPUT_BUTTON_COLOR,
@@ -32,6 +38,7 @@ function TypeList({ types, select }) {
     transition: "0.3s",
   }
 
+  // Styles for the label
   const labelStyle = {
     position: "absolute",
     top: "50%",
@@ -43,14 +50,23 @@ function TypeList({ types, select }) {
     cursor: "pointer"
   }
 
+  // Retrieve the state of vehicleTypeSelected and serviceTypeSelected from context to calculate the price
+  // setServiceCost are also retrieved from context to reset the service cost when a new type is selected
+  // setDateTimeSelected is also retrieved from context to deselect the date and time when a new type is selected
   const { vehicleTypeSelected, serviceTypeSelected, setServiceCost, setDateTimeSelected } = useContext(BookingContext)
   
+  // Calculate the price when a new type is selected and set the service cost in context
   setServiceCost(calculatePrice(vehicleTypeSelected, serviceTypeSelected))
   
+  // Function to handle the change of the type selected
   const handleChange = (e) => {
+    // Set the state of selected to the value of the type selected
     setSelected(e.currentTarget.value)
+    // Call the function received as a prop from the parent component to set the state in context
     select(e.currentTarget.value)
+    // Calculate the price when a new type is selected and set the service cost in context
     setServiceCost(calculatePrice(vehicleTypeSelected, serviceTypeSelected))
+    // Deselect the date and time when a new type is selected
     setDateTimeSelected("")
   }
 
@@ -58,20 +74,21 @@ function TypeList({ types, select }) {
   return (
     <div id="type-list" className="items-center justify-between mt-2">
         <ul className="flex gap-2">
-          {types.types.map( type => (
+          {types.types.map( type => ( // Map through the types list and return a list item for each type
             <li 
               className="border-2 w-1/4 text-center"
-              key={ type.id }
-              style={isHovered === type.id ? liHoveredStyle : liStyle}
-              onMouseEnter={() => setIshovered(type.id)}
-              onMouseLeave={() => setIshovered(null)}>
-              <input
-                className="hidden"
-                type="radio" id={type.id}
+              key={ type.id } 
+              style={isHovered === type.id ? liHoveredStyle : liStyle} // Change the style of the list item when hovered over
+              onMouseEnter={() => setIshovered(type.id)} // Set the state of isHovered to the id of the type hovered over
+              onMouseLeave={() => setIshovered(null)}> {/* Set the state of isHovered to null when the mouse leaves the type */}
+              <input 
+                className="hidden" // Hide the radio button
+                type="radio"
+                id={type.id}
                 name={types.name}
                 value={type.name}
-                onChange={handleChange}
-                checked={ selected ===  type.name } />
+                onChange={handleChange} // Call the handleChange function when the type is selected
+                checked={ selected === type.name } /> {/* Check if the type is selected */}
               <label 
                 htmlFor={type.id}
                 className="flex flex-col items-center justify-center"
@@ -79,7 +96,6 @@ function TypeList({ types, select }) {
                 <img 
                   src={type.icon}
                   alt="" />
-
                 {/* Change this to something more sustainable */}
                 <span style={{ fontSize: type.name === "Large SUV / Truck" && "10px" }}>{type.name}</span>
               </label>
